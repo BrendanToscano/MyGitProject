6483ee6d12476c44f0bd13709d0264b700cf2d19
Brendan <brendan@tgk30.com>
2024-02-05 18:20:27 -0400

    update

--- program2.py
+++ program2.py
@@ -4,6 +4,14 @@
 # Keep a loop
 # If user is done with program exit.
 # Catch errors.
+while(true):
+    try:
+        print("1. Enter 1 if you want to enter a prime number.")
+        print("2. Enter 2 if you want to exit program.")
+        num = int(input("Enter choice: -"))
+        
+    except as e:
+        print("{e}")
 
 # Check if number is prime by using %.
 
