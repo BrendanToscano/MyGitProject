9816d17877753a39ac63e56b454e56c40605acdf
Brendan <brendan@tgk30.com>
2024-02-05 18:17:03 -0400

    update

--- program2.py
+++ program2.py
@@ -1,1 +1,9 @@
+# Python program to check if the user input is a prime number.
+
+# Ask user for number.
+# Keep a loop
+# If user is done with program exit.
+# Catch errors.
+
+# Check if number is prime by using %.
 
