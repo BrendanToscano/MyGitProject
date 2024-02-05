88ccadee3c5199fffa81e29826444a1735fb70ef
Brendan <brendan@tgk30.com>
2024-02-05 18:14:00 -0400

    update

--- program1.py
+++ program1.py
@@ -1,1 +1,15 @@
+# Python program to display all the prime numbers within an interval.
 
+# Ask user for interval.
+# Catch errors.
+try:
+    startNum = int(input("Enter a starting range:- "))
+    endNum = int(input("Enter a ending range:- "))
+except:
+    print("Input was invalid.")
+
+# Run through all numbers in given range.
+for i in range(startNum, endNum):
+    # Check if the all numbers in the range are prime or not.
+
+    # If number is prime print it.
