7be307b91fdae0489cd5158d69626cb772df499c
Brendan <brendan@tgk30.com>
2024-02-05 21:25:39 -0400

    update

--- program2.py
+++ program2.py
@@ -4,39 +4,38 @@
 # Keep a loop
 # If user is done with program exit.
 # Catch errors.
-while(true):
+while True:
     try:
         print("1. If you want to check if a prime number.")
         print("2. If you want to exit program.")
         choice = input("Enter choice: -")
         
         # Choice menu
-        if choice == 1:
+        if choice == "1":
             num = int(input("Enter number: -"))
             ###### Teammate 2 would proceed from here. ######
             ###### Since team was not involved I will do this part #####
             # Check if number is prime by using %.
-                if i <= 1:
-                    isPrime = False
-                else:
-                    for j in range(2, i-1):
-                        if i % j == 0:
-                            isPrime = False
-                            break
-                if isPrime:
-                    print(f"{i} is a prime number.")
-                else:
-                    print(f"{i} is not a prime number.")
-        elif choice == 2:
+            isPrime = True
+            if num <= 1:
+                isPrime = False
+            else:
+                for i in range(2, num-1):
+                    if num % i == 0:
+                        isPrime = False
+                        break
+            if isPrime:
+                print(f"{num} is a prime number.")
+            else:
+                print(f"{num} is not a prime number.")
+        elif choice == "2":
             print("Program ended successfully.")
             break
         else:
-            print("Program ended unsuccessfully.")
+            print("Invalid choice, try again.")
             break
             
-        
-        
-    except as e:
-        print("{e}")
+    except Exception as e:
+        print(e)
 
 
