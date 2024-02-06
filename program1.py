594f5a15b328ab3fad649395fd39cff9659a5ca6
Brendan <brendan@tgk30.com>
2024-02-05 21:08:33 -0400

    update

--- program1.py
+++ program1.py
@@ -2,18 +2,28 @@
 
 # Ask user for interval.
 # Catch errors.
-try:
-    startNum = int(input("Enter a starting range:- "))
-    endNum = int(input("Enter a ending range:- "))
-except:
-    print("Input was invalid.")
+while True:
+    try:
+        startNum = int(input("Enter a starting range:- "))
+        endNum = int(input("Enter a ending range:- "))
+        break;
+    except:
+        print("Input was invalid.")
 
 # Run through all numbers in given range.
-for i in range(startNum, endNum):
+for i in range(startNum, endNum + 1):
     ###### Teammate 1 was suppose to do this part. ######
     ###### Since team was not involved I will do this part #####
     # Check if the all numbers in the range are prime or not.
-    if i > 1:
-        for j in range(
-
+    isPrime = True
+    if i <= 1:
+        isPrime = False
+    else:
+        for j in range(2, i-1):
+            if i % j == 0:
+                isPrime = False
+                break
+    
     # If number is prime print it.
+    if isPrime:
+        print(f"{i} \n")
